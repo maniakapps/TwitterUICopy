d232423ac6a95e9101fb267e75990ba54cbd8338
Manuel Pizano <mpizano@maniakapps.com>
2022-08-13 12:58:47 -0500

    Initial Commit

---
+++ TwitterUICopy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterUICopy
+//
+//  Created by Manuel pizano on 13/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterUICopy/TwitterUICopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterUICopyApp.swift
+//  TwitterUICopy
+//
+//  Created by Manuel pizano on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterUICopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
